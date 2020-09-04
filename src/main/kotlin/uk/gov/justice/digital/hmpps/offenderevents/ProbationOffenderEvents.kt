e1f87cc7e1cf402803ad8e825994af3d80033920
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-09-04 13:58:33 +0100

    DT-969 Remove unrequired exclusion

--- src/main/kotlin/uk/gov/justice/digital/hmpps/offenderevents/ProbationOffenderEvents.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/offenderevents/ProbationOffenderEvents.kt
@@ -1,14 +1,9 @@
 package uk.gov.justice.digital.hmpps.offenderevents
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.data.elasticsearch.ReactiveElasticsearchRepositoriesAutoConfiguration
-import org.springframework.boot.autoconfigure.data.elasticsearch.ReactiveElasticsearchRestClientAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    exclude = [ReactiveElasticsearchRestClientAutoConfiguration::class,
-      ReactiveElasticsearchRepositoriesAutoConfiguration::class]
-)
+@SpringBootApplication()
 class ProbationOffenderEvents
 
 fun main(args: Array<String>) {
