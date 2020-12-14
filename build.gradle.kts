a11ff13239f87c8f0d5ac20f1cd4984f9d1df0ae
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2020-12-14 09:17:39 +0000

    DT-1398 Use latest gradle plugin (#38)
    * DT-1398 Use latest gradle plugin
    
    * DT-1398 Upgrade to latest kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.1.2"
-  kotlin("plugin.spring") version "1.4.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.1.0"
+  kotlin("plugin.spring") version "1.4.21"
 }
 
 configurations {
@@ -13,17 +13,17 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
-  implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.4.RELEASE")
+  implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.5.RELEASE")
 
   implementation("io.micrometer:micrometer-registry-prometheus")
   implementation("io.micrometer:micrometer-core")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.4.8")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.4.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.4.8")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.1")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.1")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.1")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.19.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.22.0")
   testImplementation("com.google.code.gson:gson:2.8.6")
 }
