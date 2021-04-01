a837d51f4fc805e7e8aa283297440b88b445326f
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-04-01 11:47:16 +0100

    DT-1829 Upgrades (#53)
    * DT-1829 useLatestVersions
    
    * DT-1829 upgrade kotlin spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
-  kotlin("plugin.spring") version "1.4.30"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.4"
+  kotlin("plugin.spring") version "1.4.32"
 }
 
 configurations {
@@ -18,12 +18,12 @@ dependencies {
   implementation("io.micrometer:micrometer-registry-prometheus")
   implementation("io.micrometer:micrometer-core")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.4")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.24.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
   testImplementation("com.google.code.gson:gson:2.8.6")
 }
