55e51fdcefd721074f24ec71daab5d2eaa5585fe
Peter Phillips <peter@greenthistle.co.uk>
2021-04-21 13:36:48 +0100

    DT-1949: ⬆️ Update to latest localstack

--- src/test/resources/localstack/setup-sns.sh
+++ src/test/resources/localstack/setup-sns.sh
@@ -6,11 +6,11 @@ export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 export PAGER=
 
-aws --endpoint-url=http://localhost:4575 sns create-topic --name probation_offender_events
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name test_queue
-aws --endpoint-url=http://localhost:4575 sns subscribe \
+aws --endpoint-url=http://localhost:4566 sns create-topic --name probation_offender_events
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name test_queue
+aws --endpoint-url=http://localhost:4566 sns subscribe \
     --topic-arn arn:aws:sns:eu-west-2:000000000000:probation_offender_events \
     --protocol sqs \
-    --notification-endpoint http://localhost:4576/queue/test_queue
+    --notification-endpoint http://localhost:4566/queue/test_queue
 
 echo "SNS Configured"
