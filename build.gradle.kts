15ebf5e7cc2ebe46d1011b8a0ad737f9dfa9122e
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2021-12-20 14:54:05 +0000

    DT-2964: ⬆️ Upgrade hmpps-gradle-springboot plugin to 4.0.0 (#100)
    * DT-2964: ⬆️ Upgrade hmpps-gradle-springboot plugin to 4.0.0
    includes springboot 2.6.1 and kotlin 1.6
    
    * DT-2964: 🐛 Switch to different info path after upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.16"
-  kotlin("plugin.spring") version "1.5.31"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0"
+  kotlin("plugin.spring") version "1.6.0"
 }
 
 configurations {
@@ -12,23 +12,22 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.3")
 
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
   implementation("io.micrometer:micrometer-registry-prometheus")
   implementation("io.micrometer:micrometer-core")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.10")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.10")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.2")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.2")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.2")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
+  testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
-  testImplementation("com.google.code.gson:gson:2.8.8")
-  testImplementation("org.testcontainers:localstack:1.16.0")
-  testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+  testImplementation("com.google.code.gson:gson:2.8.9")
+  testImplementation("org.testcontainers:localstack:1.16.2")
 }
 
 java {
