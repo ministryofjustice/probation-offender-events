ac6bd03f21dbcfb397926e5961ac7a1a5e1f932d
marcus.aspin <maspin@unilink.com>
2022-03-29 10:06:24 +0100

    Bump Jackson to 2.13.2.2
    This includes a fix for vulnerability CVE-2020-36518.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
+ext["jackson.version.databind"] = "2.13.2.2" // Overriding Jackson databind version to fix CVE-2020-36518
+
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
