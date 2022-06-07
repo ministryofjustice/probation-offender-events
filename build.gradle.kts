86887046e630f7ae0722b2b29d0bffff6ed53b8e
Marcus Aspin <maspin@unilink.com>
2022-06-07 10:34:17 +0100

    Bump springdoc-openapi to 1.6.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,8 @@ dependencies {
   implementation("io.micrometer:micrometer-core")
 
   implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.8")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
